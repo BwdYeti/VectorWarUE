8615f5fc1083d7125c552ff691361451ba546404
BwdYeti <BwdYeti@gmail.com>
2025-05-06 13:04:18 -0400

    GGPOUE Updated
    -Plugin renamed
    -Fix calls to the plugin

--- Source/VectorWarUE4/VectorWarUE4.Build.cs
+++ Source/VectorWarUE4/VectorWarUE4.Build.cs
@@ -10,7 +10,7 @@ public class VectorWarUE4 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GGPOUE4" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GGPOUE" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
